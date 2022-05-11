18e6663884cee20b489ee2fd12433493b85145d9
coehoan <coehoan@naver.com>
2022-05-11 12:40:41 +0900

    몽고DB+스프링부트 JUnit5로 통합테스트 및 네이티브쿼리 사용해보기

---
+++ src/main/java/site/metacoding/mongotest/domain/Naver.java
@@ -0,0 +1,22 @@
+package site.metacoding.mongotest.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document(collection = "navers") // greendb/navers
+public class Naver {
+    @Id
+    private String _id;
+
+    private String company;
+
+    private String title;
+
+}
